a1e2d522383c034447f0d0d558cdf41bab30b52e
Gianluca Esposito <gianluca.esposito@oslo.it>
2017-04-14 16:31:50 +0200

    generator-cg-angular - fixed: modal template and controller N.B. both use controllerAs syntax

--- modal/templates/modal.js
+++ modal/templates/modal.js
@@ -1,4 +1,25 @@
-angular.module('<%= appname %>').controller('<%= ctrlname %>',function($scope){
+(function() {
+    'use-strict';
 
+    angular
+    .module('<%= appname %>')
+    .controller('<%= ctrlname %>', function(){
+        // PRIVATE
+        var vm = this;
 
-});
+        // PUBLIC
+        vm.ok = _ok;
+        vm.cancel = _cancel;
+
+        // PUBLIC FUNCTIONS IMPLEMENTATION
+        function _ok() {
+            
+        }
+
+        function _cancel() {
+            
+        }
+
+        // PRIVATE FUNCTIONS
+    });
+})();
